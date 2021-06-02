fb05dc5ca7b7a4b9c05a5801674cd929ecc3797a
Professor Akram <professorakramburris@gmail.com>
2021-06-01 20:01:01 -0500

    Dialogue Action
    Created Dialogue Action
    Created Dialogue UI and class
    Set up dialogue fade in and out

---
+++ 3d Playground/Assets/3D_Playground/Scripts/Conditions/Actions/DialogueAction.cs
@@ -0,0 +1,67 @@
+﻿/*****
+ * Author: Akram Taghavi-Burris
+ * Date Created: June 1, 2021
+ * Last Updated: June 2, 2021
+ * Description: Sets up the fade in and out for the dialogue group
+ * Project: 3D Playground - a drag and drop framework for 3d game development derived from Unity's own 2D Playground framework.
+ ****/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[AddComponentMenu("3D Playground/Actions/Dialogue")]
+public class DialogueAction : Action
+{
+	//reference to dialogue group
+	GameObject goDialogue; 
+
+
+
+    [Header("Dialogue UI")]
+    public GameObject dialogueUI;
+    RectTransform panel;
+    TMP_Text textBox;
+	Button btn;
+    TMP_Text btnTXT;
+
+    [Header("Dialogue Text")]
+    public string dialogueText;
+
+
+	//ExecuteAction runs the action and must return a true or false.
+	public override bool ExecuteAction(GameObject other)
+    {
+        //create instance of dialogue group
+        goDialogue = Instantiate(dialogueUI);
+
+		//set the dialogue
+		setDialogue();
+		setButton();
+
+        return true;
+    }
+
+	//set the dialogue text to display [UPDATE IN FUTURE TO ACCEPT ARRAYS]
+    void setDialogue()
+    {
+        //Get the text box of game object Dialogue
+        textBox = goDialogue.GetComponent<DialogueUI>().dialogueTextBox;
+        textBox.text = dialogueText;//set the text value
+
+
+    }
+
+    //sets the button text for the dialogue [UPDATE IN FUTURE TO CHANGE BASED ON AMOUNT OF DIALOGUE]
+	void setButton()
+    {
+		btn = goDialogue.GetComponent<DialogueUI>().button;
+        btnTXT = btn.GetComponentInChildren<TMP_Text>();
+        btnTXT.text = "x";
+	}
+
+
+
+}
---
+++ 3d Playground/Assets/3D_Playground/_INTERNAL_/Scripts/UserInterface/DialogueUI.cs
@@ -0,0 +1,88 @@
+﻿/*****
+ * Author: Akram Taghavi-Burris
+ * Date Created: June 1, 2021
+ * Last Updated: June 2, 2021
+ * Description: Sets up the fade in and out for the dialogue group
+ * Project: 3D Playground - a drag and drop framework for 3d game development derived from Unity's own 2D Playground framework.
+ ****/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueUI : MonoBehaviour
+{
+    CanvasGroup dialgueGroup;
+    public RectTransform panel;
+    public TMP_Text dialogueTextBox;
+    public Button button;
+
+
+    [Header("Dialogue Animation") ]
+    [Tooltip("Set ture to fade in, false to fade out")]
+    public bool fadeIn= true; 
+    [Tooltip("Amount of time for fade animation")]
+    public float fadeDuration = 1.5f;
+
+    float timeElapsed;
+    float transparent = 0;
+    float opaque = 1;
+    float startValue;
+    float endValue;
+
+    private void Awake()
+    {
+        dialgueGroup = GetComponent<CanvasGroup>();
+        dialgueGroup.alpha = transparent;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FadeDialogue());
+        Debug.Log(dialgueGroup);
+
+    }
+
+    private void CloseDialogue()
+    {
+        StartCoroutine(FadeDialogue());
+        Destroy(this);
+    }
+
+    // Dialogue box fades in and out
+  IEnumerator FadeDialogue()
+     {// fade from opaque to transparent
+        Debug.Log("start fade");
+
+        timeElapsed = 0;
+
+        //if sets the fade in or out
+        if (fadeIn)
+        { //time to fade in
+            startValue = transparent;
+            endValue = opaque;
+            fadeIn = false;
+        }
+        else
+        {
+            startValue = opaque;
+            endValue = transparent;
+            fadeIn = true;
+        }
+
+        //While loop runs the animation over time
+            while (timeElapsed < fadeDuration)
+            {
+                dialgueGroup.alpha = Mathf.Lerp(startValue, endValue, timeElapsed / fadeDuration);
+                timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+            dialgueGroup.alpha = endValue;
+            yield return null;
+        }//end FadeDialogue()
+       
+}
